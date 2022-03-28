3406199b1f4aa3f885ec479295cf665304b12a09
Udi Oron <udioron@gmail.com>
2022-03-28 16:58:28 +0300

    WIP templates

--- expenses/views.py
+++ expenses/views.py
@@ -1,4 +1,8 @@
+from calendar import HTMLCalendar
+
 from django.shortcuts import render
+
+from expenses.models import Expense
 
 
 def expense_list(request):
@@ -9,12 +13,16 @@ def expense_list(request):
         "bananas",
         "melon",
     ]
+    qs = Expense.objects.all()
     return render(
         request,
         "expenses/expense_list.html",
         {
+            "object_list": qs,
             "kuku123": 456789,
             "foo19": "shalom    <b>hi</b>",
             "products": products,
+            "nums": [(x, x**2) for x in range(10)],
+            "cal": HTMLCalendar().formatmonth(2022, 3),
         },
     )

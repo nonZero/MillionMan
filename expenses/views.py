fc0ebb0c16533749f5d67cfeb2430a0244c65e2d
Udi Oron <udioron@gmail.com>
2022-04-04 10:01:57 +0300

    fix radio button example

--- expenses/views.py
+++ expenses/views.py
@@ -8,6 +8,7 @@ from expenses.models import Expense
 
 def expense_list(request: HttpRequest):
     qs = Expense.objects.all()
+    # print(request.GET.getlist("my_search_fields"))
     if q := request.GET.get("q", "").strip():
         qs = qs.filter(title__icontains=q)
     return render(

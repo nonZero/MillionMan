838e6796f865a6cb01b48a4745f7f89dfd11a5c4
Udi Oron <udioron@gmail.com>
2022-04-04 09:51:08 +0300

    WIP: search form

--- expenses/views.py
+++ expenses/views.py
@@ -7,7 +7,16 @@ from expenses.models import Expense
 
 
 def expense_list(request: HttpRequest):
+    # assert False, request.GET["q"]
     qs = Expense.objects.all()
+    # if "q" in request.GET:
+    #     qs = qs.filter(title__icontains=request.GET["q"])
+    # q = request.GET.get("q", "").strip()
+    # if q:
+    #     qs = qs.filter(title__icontains=q)
+
+    if q := request.GET.get("q", "").strip():
+        qs = qs.filter(title__icontains=q)
     return render(
         request,
         "expenses/expense_list.html",

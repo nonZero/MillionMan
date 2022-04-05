784a6a77402bbd2d38bf082ac5056a1324ee681e
Shi Sherebrin <shish@mobileye.com>
2022-04-05 14:07:08 +0300

    filter by selected fields
    NOTE: value attribute of each checkbox must now match the name of its corresponding field
    - this could be avoided if needed by mapping field name to checkbox attribute

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,6 @@
+from functools import reduce
+import operator
+
 from django.db.models import Q
 from django.http import HttpRequest, Http404
 from django.shortcuts import render, get_object_or_404, redirect
@@ -22,13 +25,20 @@ AMOUNT_RANGES_Q = {
 
 def expense_list(request: HttpRequest):
     qs = Expense.objects
-    if q := request.GET.get("q", "").strip():
-        qs = qs.filter(title__icontains=q)
     if amount_range := request.GET.get("amount_range", ""):
         arq = AMOUNT_RANGES_Q.get(amount_range)
         if arq:
             qs = qs.filter(arq)
-    search_fields = request.GET.getlist("my_search_fields")
+
+    search_fields = []
+    if q := request.GET.get("q", "").strip():
+        if not (search_fields := request.GET.getlist("my_search_fields")):
+            # if fields to search were not specified via checkboxes, search them all
+            search_fields = [f.name for f in Expense._meta.get_fields()]  # QUESTION: use fields or get_fields()?
+        search_clauses = [(f'{fld}__icontains', q) for fld in search_fields]
+        q_list = [Q(clause) for clause in search_clauses]
+        qs = qs.filter(reduce(operator.or_, q_list))
+
     total = sum(
         o.amount for o in qs.all()
     )  # TODO: use SQL based aggregation==sum(amount)

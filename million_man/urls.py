7f5a3c9cafbac7a9881c548a1bc7b7128cf316bf
Udi Oron <udioron@gmail.com>
2022-04-11 12:25:10 +0300

    added login+logout

--- million_man/urls.py
+++ million_man/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 # URLS / URLCONF
 urlpatterns = [
     path("", include("expenses.urls")),
+    path("", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
 ]
